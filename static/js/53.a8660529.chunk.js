15e11d4be06ff9c8b92de3491f08ff5b1d3ff94b
yozu <yasuo@ozu.email>
2021-09-14 02:56:24 +0900

    init

---
+++ static/js/53.a8660529.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpmyApp=this.webpackJsonpmyApp||[]).push([[53],{1443:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return c}));var i=e(16),o=e(15),c=function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.h)((function(){var t=n.innerWidth,e=n.innerHeight,c=document.elementFromPoint(t/2,e/2);if(c){var r=c.closest("ion-content");r&&new Promise((function(n){return Object(o.c)(r,n)})).then((function(){Object(i.f)((function(){return r.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.a8660529.chunk.js.map
